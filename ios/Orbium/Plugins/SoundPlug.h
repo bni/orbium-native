d902e484a2e13b83afeb73fc785dc4d3090a60c0
bni <bni@jsway.se>
2011-11-26 19:42:34 +0100

    Remove stale files

--- ios/Orbium/Plugins/SoundPlug.h
+++
@@ -1,9 +0,0 @@
-#import <AudioToolbox/AudioServices.h>
-#import "PhoneGapCommand.h"
-
-@interface SoundPlug : PhoneGapCommand {
-}
-
-- (void) play:(NSMutableArray*)arguments withDict:(NSMutableDictionary*)options;
-
-@end
